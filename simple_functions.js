c8b3f4e1a5d78459121d17f69b785892b18fe0b3
Julle Nguyen <jnguyen@appdev.yearup.org>
2023-05-01 10:32:16 -0400

    simple_functions.js exerise completed

---
+++ simple_functions.js
@@ -0,0 +1,28 @@
+//define three functions:
+//1. Name favorite things and use console log 3 times
+//   display three lines listing your name, fave movie and fave music artist/s
+//2. Why I am here function 
+//   display a sentence that describes why you joined "Learn to Code"
+//3. Fave place
+//   display and descibe your favorite place to visit and why
+
+function favoriteThings(){
+    console.log("My name: Julle")
+    console.log("Favorite movie: Howl's Moving castle or anything Ghibli")
+    console.log("Favorite Musician: Danelia Andrea")
+}
+
+favoriteThings();
+
+function whyImHere(){
+    console.log("I am in LTC to have a brighter future.")
+}
+
+whyImHere();
+
+function favoritePlace(){
+    console.log("Location: Flordia")
+    console.log("Why? : I feel like the gulf rejuvenates my soul.")
+}
+
+favoritePlace();
