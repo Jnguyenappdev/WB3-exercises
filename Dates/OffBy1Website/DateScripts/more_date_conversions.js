48cdaf8e32fa8dfa7e7af98eb39a036ebef16279
Julle Nguyen <jnguyen@appdev.yearup.org>
2023-05-03 11:43:28 -0400

    Update 2

--- Dates/OffBy1Website/DateScripts/more_date_conversions.js
+++
@@ -1,9 +0,0 @@
-const myBday = new Date('2000-03-05')
-const momBday = new Date('1971-03-25')
-const dadBday =new Date('1966-02-20')
-
-console.log(myBday.toLocaleDateString())
-
-console.log(momBday.toLocaleDateString())
-
-console.log(dadBday.toLocaleDateString())
